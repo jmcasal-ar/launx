8947cc175589ee13e7f8e2fdaa0b65f8bd0375cd
jmcasal-ar <casaljuanmanuel@gmail.com>
2023-12-28 14:54:09 -0300

    agregando cache en la api, corrigiendo redes y agregando mail institucional

--- UniLab/courses/views.py
+++ UniLab/courses/views.py
@@ -1,5 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from decimal import Decimal  # Añade esta línea al inicio de tu archivo
+from django.core.cache import cache
+from datetime import datetime, timedelta
 from django.shortcuts import render
 from .models import Course
 from django.shortcuts import render
@@ -24,7 +27,7 @@ class PaymentDetailView(DetailView):
             response = requests.get(url_api_bitcoin)
             response.raise_for_status()
             data = response.json()
-            precio_bitcoin_usd = data["bitcoin"]["usd"]
+            precio_bitcoin_usd = data.get("bitcoin", {}).get("usd")  # Usa get para manejar claves ausentes
             return precio_bitcoin_usd
         except requests.exceptions.RequestException as e:
             print(f"Error al obtener el precio del Bitcoin: {e}")
@@ -36,7 +39,7 @@ class PaymentDetailView(DetailView):
             response = requests.get(url_api_ethereum)
             response.raise_for_status()
             data = response.json()
-            precio_ethereum_usd = data["ethereum"]["usd"]
+            precio_ethereum_usd = data.get("ethereum", {}).get("usd")  # Usa get para manejar claves ausentes
             return precio_ethereum_usd
         except requests.exceptions.RequestException as e:
             print(f"Error al obtener el precio del Ethereum: {e}")
@@ -44,8 +47,30 @@ class PaymentDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        precio_bitcoin = self.obtener_precio_bitcoin()
-        precio_ethereum = self.obtener_precio_ethereum()
+
+        # Claves para la caché
+        btc_cache_key = 'btc_price'
+        eth_cache_key = 'eth_price'
+
+        # Obtener precios almacenados en caché
+        precio_bitcoin = cache.get(btc_cache_key)
+        precio_ethereum = cache.get(eth_cache_key)
+
+        # Verificar si los precios están almacenados en caché y si han pasado 24 horas
+        if precio_bitcoin is None or precio_ethereum is None or \
+                cache.get('last_update_time') is None or \
+                (datetime.now() - cache.get('last_update_time')).days >= 1:
+
+            # Obtener los precios actualizados
+            precio_bitcoin = self.obtener_precio_bitcoin()
+            precio_ethereum = self.obtener_precio_ethereum()
+
+            # Guardar precios en caché
+            cache.set(btc_cache_key, precio_bitcoin)
+            cache.set(eth_cache_key, precio_ethereum)
+
+            # Actualizar el tiempo de la última actualización
+            cache.set('last_update_time', datetime.now())
 
         if precio_bitcoin is not None:
             precio_bitcoin = round(float(precio_bitcoin), 2)
@@ -53,15 +78,13 @@ class PaymentDetailView(DetailView):
         if precio_ethereum is not None:
             precio_ethereum = round(float(precio_ethereum), 2)
 
-        # División del precio del dólar por el precio del bitcoin
-        if self.object.dollarPrice is not None and precio_bitcoin != 0:
-            context['precio_bitcoin'] = round(float(self.object.dollarPrice) / precio_bitcoin, 8)
+        if self.object.dollarPrice is not None and precio_bitcoin is not None and precio_bitcoin != 0:
+            context['precio_bitcoin'] = round(Decimal(self.object.dollarPrice) / Decimal(precio_bitcoin), 8)
         else:
             context['precio_bitcoin'] = None
 
-        # División del precio del dólar por el precio del ethereum
-        if self.object.dollarPrice is not None and precio_ethereum != 0:
-            context['precio_ethereum'] = round(float(self.object.dollarPrice) / precio_ethereum, 8)
+        if self.object.dollarPrice is not None and precio_ethereum is not None and precio_ethereum != 0:
+            context['precio_ethereum'] = round(Decimal(self.object.dollarPrice) / Decimal(precio_ethereum), 8)
         else:
             context['precio_ethereum'] = None
 
