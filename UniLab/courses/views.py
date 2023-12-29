bee2f24818f773f00d320225efd22225c6b3ed07
Juan Manuel Casal <juan.manuel.casal@istea.com.ar>
2023-12-28 23:31:07 -0300

    agregnado manejo de errores de btc y eth

--- UniLab/courses/views.py
+++ UniLab/courses/views.py
@@ -27,7 +27,7 @@ class PaymentDetailView(DetailView):
             response = requests.get(url_api_bitcoin)
             response.raise_for_status()
             data = response.json()
-            precio_bitcoin_usd = data.get("bitcoin", {}).get("usd")
+            precio_bitcoin_usd = data.get("bitcoin", {}).get("usd")  # Usa get para manejar claves ausentes
             return precio_bitcoin_usd
         except requests.exceptions.RequestException as e:
             print(f"Error al obtener el precio del Bitcoin: {e}")
@@ -39,7 +39,7 @@ class PaymentDetailView(DetailView):
             response = requests.get(url_api_ethereum)
             response.raise_for_status()
             data = response.json()
-            precio_ethereum_usd = data.get("ethereum", {}).get("usd")
+            precio_ethereum_usd = data.get("ethereum", {}).get("usd")  # Usa get para manejar claves ausentes
             return precio_ethereum_usd
         except requests.exceptions.RequestException as e:
             print(f"Error al obtener el precio del Ethereum: {e}")
@@ -48,32 +48,22 @@ class PaymentDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # Claves para la caché
         btc_cache_key = 'btc_price'
         eth_cache_key = 'eth_price'
 
-        # Obtener precios almacenados en caché
         precio_bitcoin = cache.get(btc_cache_key)
         precio_ethereum = cache.get(eth_cache_key)
 
-        # Verificar si los precios están almacenados en caché y si han pasado 24 horas
         if precio_bitcoin is None or precio_ethereum is None or \
                 cache.get('last_update_time') is None or \
                 (datetime.now() - cache.get('last_update_time')).days >= 1:
 
-            try:
-                # Obtener los precios actualizados
-                precio_bitcoin = self.obtener_precio_bitcoin()
-                precio_ethereum = self.obtener_precio_ethereum()
+            precio_bitcoin = self.obtener_precio_bitcoin()
+            precio_ethereum = self.obtener_precio_ethereum()
 
-                # Guardar precios en caché
-                cache.set(btc_cache_key, precio_bitcoin)
-                cache.set(eth_cache_key, precio_ethereum)
-
-                # Actualizar el tiempo de la última actualización
-                cache.set('last_update_time', datetime.now())
-            except Exception as e:
-                print(f"Error al actualizar la caché: {e}")
+            cache.set(btc_cache_key, precio_bitcoin)
+            cache.set(eth_cache_key, precio_ethereum)
+            cache.set('last_update_time', datetime.now())
 
         if precio_bitcoin is not None:
             precio_bitcoin = round(float(precio_bitcoin), 2)
@@ -82,16 +72,21 @@ class PaymentDetailView(DetailView):
             precio_ethereum = round(float(precio_ethereum), 2)
 
         if self.object.dollarPrice is not None and precio_bitcoin is not None and precio_bitcoin != 0:
-            context['precio_bitcoin'] = round(Decimal(self.object.dollarPrice) / Decimal(precio_bitcoin), 8)
+            try:
+                context['precio_bitcoin'] = round(Decimal(self.object.dollarPrice) / Decimal(precio_bitcoin), 8)
+            except DecimalException as e:
+                print(f"Error al calcular el precio en Bitcoin: {e}")
+                context['precio_bitcoin'] = "No disponible"
         else:
-            context['precio_bitcoin'] = None
+            context['precio_bitcoin'] = "No disponible"
 
         if self.object.dollarPrice is not None and precio_ethereum is not None and precio_ethereum != 0:
-            context['precio_ethereum'] = round(Decimal(self.object.dollarPrice) / Decimal(precio_ethereum), 8)
+            try:
+                context['precio_ethereum'] = round(Decimal(self.object.dollarPrice) / Decimal(precio_ethereum), 8)
+            except DecimalException as e:
+                print(f"Error al calcular el precio en Ethereum: {e}")
+                context['precio_ethereum'] = "No disponible"
         else:
-            context['precio_ethereum'] = None
-
-        print(f"self.object.dollarPrice: {self.object.dollarPrice}")
-        print(f"precio_bitcoin: {precio_bitcoin}")
+            context['precio_ethereum'] = "No disponible"
 
         return context
